f485def2830b9fb070cca93a834730d0da115047
FernandoHenrique06 <137003578+FernandoHenrique06@users.noreply.github.com>
2024-10-21 11:34:40 -0300

    InfraRed.ino

---
+++ InfraRed.ino
@@ -0,0 +1,29 @@
+const int NUM_SENSORS = 8;  // Número total de sensores IR
+const int IR_PINS[NUM_SENSORS] = {2, 3, 4, 5, 6, 7, 8, 9}; // Pinos digitais
+
+
+void setup() {
+  Serial.begin(9600);
+  for (int i = 0; i < NUM_SENSORS; ++i) {
+    pinMode(IR_PINS[i], INPUT);
+  }
+}
+
+
+void loop() {
+  for (int i = 0; i < NUM_SENSORS; ++i) {
+    unsigned long duration = pulseIn(IR_PINS[i], HIGH); // Mede a duração do pulso em HIGH para o sensor atual
+   
+    if (duration > 0) {
+      float dutyCycle = (duration / 1000.0) * 100.0; // Calcula o duty cycle em percentagem
+      Serial.print(dutyCycle);
+      Serial.print(",");
+    }
+  }
+  Serial.println(); // Quebra de linha após cada ciclo de aferição dos sensores
+  delay(600);
+}
+
+
+
+
